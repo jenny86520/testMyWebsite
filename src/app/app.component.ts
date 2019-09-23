69362b8bc6993cfe6488fdd3d4bcde3cfee1dc50
呂芯萍 <aka_lu@bankpro.com.tw>
2019-09-23 16:40:07 +0800

    Add title for each page (post: 0x06)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'testMyWebsite';
   path = 'otherpath';
+
+  posts = environment.posts;
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,25 @@
 export const environment = {
-  production: true
+  production: true,
+  posts: [
+    {
+      title: 'localhost:4200',
+      path: '/'
+    },
+    {
+      title: 'Welcome to MyWebsite',
+      path: '/home'
+    },
+    {
+      title: 'What is Binding ?',
+      path: '/myPage'
+    },
+    {
+      title: 'Load Param & Show Param',
+      path: '/myPage/children'
+    },
+    {
+      title: '404 Page',
+      path: '/otherpath'
+    }
+  ]
 };
